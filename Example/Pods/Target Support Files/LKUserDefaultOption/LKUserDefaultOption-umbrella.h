4130fa23337481d654ec08646a6b0398d1732ed4
Hiroshi Hashiguchi <xcatsan@mac.com>
2015-09-24 06:42:20 +0900

    First Commit

---
+++ Example/Pods/Target Support Files/LKUserDefaultOption/LKUserDefaultOption-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double LKUserDefaultOptionVersionNumber;
+FOUNDATION_EXPORT const unsigned char LKUserDefaultOptionVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-LKUserDefaultOption_Example/Pods-LKUserDefaultOption_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LKUserDefaultOption_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LKUserDefaultOption_Example
+@end
